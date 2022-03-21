8f64aa5aa7d7157c6a83dbe1f64530c6e45f4f1f
Manesh Kumar <maneeshk5@users.noreply.github.com>
2022-03-21 12:40:48 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import Currency from './Currency';
+import Weather from "./Weather";
+import {Button, Col, Container, Row} from 'react-bootstrap';
+import {useState} from "react";
+
 
 function App() {
+  const [openCurrency,setOpenCurrency] = useState(false);
+  const [openWeather,setOpenWeather] = useState(false);
+
+  function currencyFunc(){
+    setOpenCurrency(true);
+    setOpenWeather(false);
+  }
+  function weatherFunc(){
+    setOpenCurrency(false);
+    setOpenWeather(true);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+          <Row>
+            <Col>
+              <Button className="currencyBtn" onClick={currencyFunc}>Currency Page</Button>
+              <Button onClick={weatherFunc} className="weatherBtn">Weather Page</Button>
+              {openCurrency?<Currency />:""}
+              {openWeather?<Weather />:""}
+            </Col>
+          </Row>
+        </Container>
+
       </header>
     </div>
   );
